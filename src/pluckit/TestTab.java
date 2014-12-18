b7056826f3b9ed3e46bf722d0c99fd32746ec7e6
mwillis4 <bigmw07@msn.com>
2014-12-17 23:15:57 -0700

    Update TestTab.java
    Comments

--- src/pluckit/TestTab.java
+++ src/pluckit/TestTab.java
@@ -17,9 +17,15 @@ import javafx.scene.layout.StackPane;
  * @author Douglas
  */
 public class TestTab {
-    FlowPane tab;
-    ArrayList<WritableImage> images;
     
+    // Holds all the chords
+    FlowPane tab;
+    ArrayList<WritableImage> images; // Holds all the images in the chord
+    
+    /** 
+     * returns the flow plane
+     * @return 
+     */
     public FlowPane getTab(){
         tab = new FlowPane();
         tab.setTranslateX(10);
@@ -27,31 +33,49 @@ public class TestTab {
         return tab;
     }
     
+    /**
+     * Adds a new chord to the display
+     * @param chord 
+     */
     public void getChild(int chord[]){
-          if (images == null)
+        
+        // Make sure we have a list of images
+        if (images == null)
             images = new ArrayList<WritableImage>();
+        
+        // Create the image view to take a picture of the tab when it is created
         ImageView view = new ImageView(new Image("/tab.png"));
         
-        StackPane newPane = new StackPane(view,getChord(-100,chord[0]),getChord(-60,chord[1]),getChord(-20,chord[2]),getChord(20,chord[3]),getChord(60,chord[4]),getChord(100,chord[5]));  
+        // Create the stack pane with everything positioned correctly
+        StackPane newPane = new StackPane(view,getChord(-100,chord[0]),
+                getChord(-60,chord[1]),getChord(-20,chord[2]),
+                getChord(20,chord[3]),getChord(60,chord[4]),
+                getChord(100,chord[5]));  
         
+        // Add the pane to the display
         tab.getChildren().addAll(newPane);
         tab.setHgap(20);
         tab.setVgap(20);
         
-        
+        // Add the picture of the tab to the list of images
         images.add(newPane.snapshot(null, null));
 
     }
     
-    
-    
+    /**
+     * Creates a chord image
+     * @param posX
+     * @param posY
+     * @return 
+     */    
     public ImageView getChord(int posX, int posY){
       
-        
+        // Will hold the image that is created
         ImageView image = new ImageView(getNote());
         
         int pos = 0;
         
+        // Put the fingering in the correct place
         switch(posY){
             case 0: image = new ImageView();
                 break;
@@ -71,32 +95,39 @@ public class TestTab {
                 
         }
         
+        // Position everything correctly
         image.setTranslateX(posX);
         image.setTranslateY(pos);
                 
+        // Return the image
         return image;
         
     }
     
+    /**
+     * gets the correct image for a note
+     * @return 
+     */
     public Image getNote(){
         Image note = new Image("/circle.png");
         
         return note;
     }
     
-    public WritableImage getImage(int pos) 
-    {
-        return images.get(pos);
-    }
-    
+    /**
+     * gets the number of images in the list
+     * @return 
+     */
     public int getImageCount() {
         return images.size();
     }
             
+    /**
+     * gets all the images in the list
+     * @return 
+     */
     public ArrayList<WritableImage> getImages()
     {
         return images;
     }
 }
-
-  
